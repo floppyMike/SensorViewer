ddcd8bc6fa1da42d35f5dc730f5dc1b051ae21cc
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-04-20 22:13:17 +0200

    Add a horizontal padding and a topbar

--- app/src/main/java/com/example/sensorviewer/MainActivity.kt
+++ app/src/main/java/com/example/sensorviewer/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.sensorviewer
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -9,39 +14,44 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.sensorviewer.ui.theme.SensorViewerTheme
-import androidx.core.app.ActivityCompat
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.widget.Toast
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.mutableStateOf
-import kotlinx.coroutines.delay
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import com.example.sensorviewer.ui.theme.SensorViewerTheme
+import kotlinx.coroutines.delay
 import kotlin.math.abs
 
 data class SensorData(
@@ -148,8 +158,11 @@ class MainActivity : ComponentActivity(), SensorEventListener {
                     context.startService(intent)
                 }
 
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.padding(innerPadding)) {
+                Scaffold(
+                    topBar = { TopBarDisplay() },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)) {
                         PeriodSlider(
                             period = periodState.value,
                             onPeriodChange = { periodState.value = it },
