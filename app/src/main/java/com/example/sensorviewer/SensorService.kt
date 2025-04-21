97500356b4b5d7f9f4559bd62c1412881644d236
floppyMike <36990990+floppyMike@users.noreply.github.com>
2025-04-22 00:13:53 +0200

    Add Service close

--- app/src/main/java/com/example/sensorviewer/SensorService.kt
+++ app/src/main/java/com/example/sensorviewer/SensorService.kt
@@ -1,8 +1,8 @@
 package com.example.sensorviewer
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
@@ -12,8 +12,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.launch
 import kotlin.math.roundToLong
 
@@ -21,10 +19,9 @@ class SensorService : Service() {
     companion object {
         const val CHANNEL_ID = "monitor_service_channel"
         const val NOTIFICATION_ID = 1
+        private const val ACTION_STOP_SERVICE = "com.example.sensorviewer.ACTION_STOP_SERVICE"
 
-        const val EXTRA_AX = "extra_ax"
-        const val EXTRA_AY = "extra_ay"
-        const val EXTRA_AZ = "extra_az"
+        const val EXTRA_DATA = "extra_data"
         const val EXTRA_PERIOD = "extra_period"
     }
 
@@ -39,27 +36,37 @@ class SensorService : Service() {
             CHANNEL_ID,
             "Sensor Monitoring",
             NotificationManager.IMPORTANCE_LOW
-        )
-        serviceChannel.description = "Channel for accelerometer monitor service"
+        ).apply { description = "Channel for accelerometer monitor service" }
         getSystemService(NotificationManager::class.java)
             .createNotificationChannel(serviceChannel)
 
-        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Accelerometer Monitor")
-            .setContentText("Monitoring thresholds...")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .build()
-        startForeground(NOTIFICATION_ID, notification)
+        startForeground(
+            NOTIFICATION_ID, NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Accelerometer Monitor")
+                .setContentText("Monitoring thresholds...")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .addAction(
+                    R.drawable.ic_launcher_foreground, "Stop", PendingIntent.getService(
+                        this, 0, Intent(
+                            this, SensorService::class.java
+                        ).apply { action = ACTION_STOP_SERVICE },
+                        PendingIntent.FLAG_IMMUTABLE
+                    )
+                )
+                .build()
+        )
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        intent?.let {
-            val threshold = listOf(
-                it.getFloatExtra(EXTRA_AX, 0f),
-                it.getFloatExtra(EXTRA_AY, 0f),
-                it.getFloatExtra(EXTRA_AZ, 0f),
-            )
+        if (intent?.action == ACTION_STOP_SERVICE) {
+            stopForeground(STOP_FOREGROUND_REMOVE)
+            stopSelf()
+            return START_NOT_STICKY
+        }
 
+        intent?.let {
+            val threshold = it.getFloatArrayExtra(EXTRA_DATA)?.toList()
+                ?: emptyList() // Empty list will disable it
             val period = it.getFloatExtra(EXTRA_PERIOD, 1f).roundToLong() * 1000
             var prevTime = 0L
 
@@ -68,9 +75,7 @@ class SensorService : Service() {
                     .accelerometerFlow()
                     .collect {
                         val now = System.currentTimeMillis()
-                        if (now - prevTime < period) return@collect
-
-                        if (it.zip(threshold).any { (c, t) -> c > t }) {
+                        if (now - prevTime >= period && it.zip(threshold).any { (c, t) -> c > t }) {
                             prevTime = now
                             CoroutineScope(Dispatchers.Main).launch {
                                 Toast.makeText(
